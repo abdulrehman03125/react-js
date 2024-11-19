a06c165286f908014f8923e5177e6e8cfa7b0963
Abdul Rahman <chrahman0312@gmail.com>
2024-11-19 11:29:31 +0500

    edit button used

--- Tu to app/src/App.jsx
+++ Tu to app/src/App.jsx
@@ -1,57 +1,82 @@
-import { useState } from 'react'
-import './App.css'
-import Listitem from './components/Listitem';
+import { useState } from "react";
+import "./App.css";
+import Listitem from "./components/Listitem";
 
 function App() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(null);
 
   const deleteItem = (index) => {
     const newItems = items.filter((el, i) => i != index);
     setItems(newItems);
-  }
-
-  const updateItem = (index) => {
-    const newItems = [...items];
-    newItems[index] = name;
-    setItems(newItems);
-  }
-
+  };
   const addItem = () => {
     const newItemsAr = [...items, name];
     setItems(newItemsAr);
     setName("");
-  }
+  };
+  const editItem = (index) => {
+    setName(items[index]);
+    setEditing(true);
+    setCurrentIndex(index);
+  };
+
+  const updateItem = (index) => {
+    // alert("anchor")
+    const updateItems = items.map((el, i) => (i == currentIndex) ? name : el);
+    setItems(updateItems);
+    setName("");
+    setEditing(false);
+    setCurrentIndex(null);
+  };
+
+ 
 
   return (
     <div>
-      <div className='d-flex gap-2 mb-3'>
-        <input className='form-control' onChange={(e) => {setName(e.target.value)}  } value={name} type='text' placeholder='New' />
-        <button className='btn btn-sm btn-warning' onClick={addItem}>Add</button>
-      </div>
-  <table className="table table-bordered" style={{ width: "500px" }}>
-
-<thead>
-  <tr>
-    <th scope="col">#</th>
-    <th scope="col">Item Name</th>
-    <th scope="col">Actions Delete</th>
-    <th scope="col">Actions Update</th>
-    <th scope="col">Actions Edite</th>
-  </tr>
-</thead>
-<tbody>
+      <div className="d-flex gap-2 mb-3">
+        <input
+          className="form-control"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+          type="text"
+          placeholder="New"
+        />
+        <button className="btn btn-sm btn-warning" onClick={editing == true ? updateItem : addItem}>
           {
-            items.map((item, i) => {
-              return (
-                <Listitem deleteItem={deleteItem} updateItem={updateItem} item={item} key={i} index={i} />
-              )
-            })
+          (editing == true) ? "Update" : "Add"
+          
           }
+        </button>
+      </div>
+      <table className="table table-bordered" style={{ width: "500px" }}>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Item Name</th>
+            <th scope="col">Actions Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, i) => {
+            return (
+              <Listitem
+                deleteItem={deleteItem}
+                editItem={editItem}
+                item={item}
+                key={i}
+                index={i}
+              />
+            );
+          })}
         </tbody>
-    </table>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Tu to app/src/components/Listitem.jsx
+++ Tu to app/src/components/Listitem.jsx
@@ -1,15 +1,28 @@
-
 function Listitem(props) {
-
-    return (
-         <tr>
-            <th scope="row">{props.index + 1}</th>
-            <td>{props.item}</td>
-            <td><button onClick={ () => {props.deleteItem(props.index)} } className="btn btn-sm btn-danger">Delete</button></td>
-            <td><button onClick={()=> {props.updateItem(props.index)}} className="btn btn-sm btn-danger">Edite</button></td>
-            <td><button onClick={()=> {props.updateItem(props.index)}} className="btn btn-sm btn-danger">Update</button></td>
-          </tr>
-    )
+  return (
+    <tr>
+      <th scope="row">{props.index + 1}</th>
+      <td>{props.item}</td>
+      <td>
+        <button
+          onClick={() => {
+            props.deleteItem(props.index);
+          }}
+          className="btn btn-sm btn-danger me-2"
+        >
+          Delete
+        </button>
+        <button
+          onClick={() => {
+            props.editItem(props.index);
+          }}
+          className="btn btn-sm btn-info btn-edit"
+        >
+          Edit
+        </button>
+      </td>
+    </tr>
+  );
 }
 
 export default Listitem;
